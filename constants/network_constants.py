23b0bdee8270f81d8ec913cd0d49ff4c3bfa66ad
hoangthanhlamm <hoangthanhlamm@gmail.com>
2022-03-16 16:54:27 +0700

    fix: provider

--- constants/network_constants.py
+++ constants/network_constants.py
@@ -17,9 +17,9 @@ class Chain:
 
 
 class ProviderURI:
-    bsc_provider_uri = 'https://nd-384-319-366.p2pify.com/7e49b20f53222da5f0b4517cd1da43ef'
-    ethereum_provider_uri = 'https://nd-892-894-739.p2pify.com/175a62ad96c3a9422c55f877b9ab366e'
-    ftm_provider_uri = 'https://nd-805-726-483.p2pify.com/d0a55e92a30ac535e1334e1f74196736'
+    bsc_provider_uri = 'https://bsc-dataseed.binance.org/'
+    ethereum_provider_uri = 'https://mainnet.infura.io/v3/'
+    ftm_provider_uri = 'https://rpc.ftm.tools/'
 
     mapping = {
         Chain.bsc: bsc_provider_uri,
